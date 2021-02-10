541c02331e65d4d99a8b909b2cef0ed26d3d8d65
fawzythegoat <78029053+fawzythegoat@users.noreply.github.com>
2021-02-10 20:36:48 +0200

    Add files via upload

--- rango/admin.py
+++ rango/admin.py
@@ -8,6 +8,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
 
+admin.site.register(Page, PageAdmin)
